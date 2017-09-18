18313c5339e73f82c3d14b36ad43dc9f1cd453ec
Peter van Ooy <pevanooy@gmail.com>
2017-09-18 19:38:53 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -12,12 +12,12 @@ function logWhisper(string) {
 }
 function sayHiToGrandma(string) {
   if (string === string.toLowerCase()){
-    return "I can't hear you!"
+    return "I can't hear you!";
   }
   elseif (string === string.toUpperCase()){
-      return "YES INDEED!"
+      return "YES INDEED!";
   }
   elseif (string === "I love you, Grandma.") {
-    return "I love you, too."
+    return "I love you, too.";
   }
 }

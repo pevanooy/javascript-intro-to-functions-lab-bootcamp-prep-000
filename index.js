22e993da9d100515501b9da8fd5dd36ce416d584
Peter van Ooy <pevanooy@gmail.com>
2017-09-18 19:37:17 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -14,4 +14,10 @@ function sayHiToGrandma(string) {
   if (string === string.toLowerCase()){
     return "I can't hear you!"
   }
+  elseif (string === string.toUpperCase()){
+      return "YES INDEED!"
+  }
+  elseif (string === "I love you, Grandma.") {
+    return "I love you, too."
+  }
 }
